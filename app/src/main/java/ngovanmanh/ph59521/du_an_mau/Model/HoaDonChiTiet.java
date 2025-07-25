ac6d48c4ba59c0c6f035e16327ad740a9e26e76e
ManhNgoDev <ngovanm691@gmail.com>
2025-07-25 14:33:38 +0700

    update Model DbHelper

--- app/src/main/java/ngovanmanh/ph59521/du_an_mau/Model/HoaDonChiTiet.java
+++ app/src/main/java/ngovanmanh/ph59521/du_an_mau/Model/HoaDonChiTiet.java
@@ -1,18 +1,30 @@
 package ngovanmanh.ph59521.du_an_mau.Model;
 
 public class HoaDonChiTiet {
-    private String maHoaDonChiTiet;
+    private String maCTHD;
     private String maHoaDon;
     private String maSanPham;
     private int soLuong;
     private double donGia;
 
-    public String getMaHoaDonChiTiet() {
-        return maHoaDonChiTiet;
+    public HoaDonChiTiet() {
     }
 
-    public void setMaHoaDonChiTiet(String maHoaDonChiTiet) {
-        this.maHoaDonChiTiet = maHoaDonChiTiet;
+    public HoaDonChiTiet(String maCTHD, String maHoaDon, String maSanPham, int soLuong, double donGia) {
+        this.maCTHD = maCTHD;
+        this.maHoaDon = maHoaDon;
+        this.maSanPham = maSanPham;
+        this.soLuong = soLuong;
+        this.donGia = donGia;
+    }
+
+    // getter và setter
+    public String getMaCTHD() {
+        return maCTHD;
+    }
+
+    public void setMaCTHD(String maCTHD) {
+        this.maCTHD = maCTHD;
     }
 
     public String getMaHoaDon() {
@@ -45,16 +57,5 @@ public class HoaDonChiTiet {
 
     public void setDonGia(double donGia) {
         this.donGia = donGia;
-    }
-
-    public HoaDonChiTiet(String maHoaDonChiTiet, String maHoaDon, String maSanPham, int soLuong, double donGia) {
-        this.maHoaDonChiTiet = maHoaDonChiTiet;
-        this.maHoaDon = maHoaDon;
-        this.maSanPham = maSanPham;
-        this.soLuong = soLuong;
-        this.donGia = donGia;
-    }
-
-    public HoaDonChiTiet() {
     }
 }
